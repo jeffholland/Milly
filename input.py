f0652682e45c2db7ef66fc475e8a7f0ca890e487
Jeff Holland <holland.musictech@gmail.com>
2022-12-05 08:50:26 -0500

    input clear

--- input.py
+++ input.py
@@ -5,18 +5,27 @@ from data import *
 
 class Input(tk.Frame):
     def __init__(self, width, height, bg, master=None):
-        tk.Frame.__init__(self, master, width=width, height=height, bg=bg)
+        tk.Frame.__init__(
+            self, 
+            master, 
+            width=width, 
+            height=height, 
+            bg=bg)
         self.create_widgets()
 
     def submit(self):
         add_entry(self.input.get("1.0", "end"))
         self.master.refresh_entries()
+        self.input.delete("1.0", "end")
 
     def save(self):
         print("this method will save current diary data to a JSON file")
 
     def create_widgets(self):
-        self.input = tk.Text(self, height=10, bg="grey")
+        self.input = tk.Text(
+            self, 
+            height=10, 
+            bg="grey")
         self.input.grid(row=0, column=0, padx=PADDING, pady=PADDING, rowspan=10)
 
         self.submit_button = tk.Button(self, text="Submit", command=self.submit)
