b9eec259f985c8b6aa32d33ed31265e1f01f52e4
Jeff Holland <holland.musictech@gmail.com>
2022-12-08 15:35:02 -0500

    save prompt has widgets and is right size

--- src/save_prompt.py
+++ src/save_prompt.py
@@ -8,51 +8,64 @@ class SavePrompt(tk.Frame):
 
         self.window = tk.Toplevel(master)
         self.window.title("Save prompt")
-        self.window.geometry("200x200")
+        self.window.geometry("370x100")
+
+        self.create_widgets()
 
     def create_widgets(self):
         self.label = tk.Label(
-            self,
-            text="Would you like to save changes?"
+            self.window,
+            text="Would you like to save changes?",
+            fg=colors["HL1"]
         )
         self.label.grid(
             row=0,
             column=0,
-            columnspan=4
+            columnspan=4,
+            padx=PADDING,
+            pady=PADDING
         )
 
         self.yes_button = tk.Button(
-            self,
+            self.window,
             text="Yes"
         )
         self.yes_button.grid(
             row=1,
-            column=0
+            column=0,
+            padx=PADDING,
+            pady=PADDING
         )
 
         self.no_button = tk.Button(
-            self,
+            self.window,
             text="No"
         )
         self.no_button.grid(
             row=1,
-            column=3
+            column=1,
+            padx=PADDING,
+            pady=PADDING
         )
 
         self.cancel_button = tk.Button(
-            self,
+            self.window,
             text="Cancel"
         )
         self.cancel_button.grid(
             row=1,
-            column=2
+            column=2,
+            padx=PADDING,
+            pady=PADDING
         )
 
         self.save_new_button = tk.Button(
-            self,
+            self.window,
             text="Save new"
         )
         self.save_new_button.grid(
             row=1,
-            column=1
+            column=3,
+            padx=PADDING,
+            pady=PADDING
         )
