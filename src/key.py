28cd3eb4520a512875234452f1e6ed79be85e302
Jeff Holland <holland.musictech@gmail.com>
2022-12-08 16:12:46 -0500

    changed key shortcuts

--- src/key.py
+++ src/key.py
@@ -2,19 +2,25 @@ class Key:
     def __init__(self, master):
         self.master = master
         self.keys_pressed = {
-            "cmd": False
+            "cmd": False,
+            "shift": False
         }
         self.just_submitted = False
 
     def key_press(self, event):
 
-        if event.keysym == "Meta_L" or event.keysym == "Meta_R":
+        # Holding down CMD or Shift
+        if "Meta" in event.keysym:
             self.keys_pressed["cmd"] = True
+        if "Shift" in event.keysym:
+            self.keys_pressed["shift"] = True
+
+        # Return to submit, shift+return for newline
+        if event.keysym == "Return" and self.keys_pressed["shift"] == False:
+            self.master.submit()
+            self.just_submitted = True
         
         if self.keys_pressed["cmd"] == True:
-            if event.keysym == "Return":
-                self.master.submit()
-                self.just_submitted = True
             if event.keysym == "s":
                 self.master.save()
             if event.keysym == "l":
@@ -23,8 +29,10 @@ class Key:
                 self.master.clear()
 
     def key_release(self, event):
-        if event.keysym == "Meta_L" or event.keysym == "Meta_R":
+        if "Meta" in event.keysym:
             self.keys_pressed["cmd"] = False
+        if "Shift" in event.keysym:
+            self.keys_pressed["shift"] = False
 
         if self.just_submitted == True:
             if event.keysym == "Return":
