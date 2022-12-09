ee5a6a4f7eca749727a524cd80a2f884c28f5cb1
Jeff Holland <holland.musictech@gmail.com>
2022-12-09 08:25:56 -0500

    fixed import statements

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 
-from constants import colors, HEIGHT, WIDTH
+from colors import colors
+from constants import HEIGHT, WIDTH
 from entries import Entries
 from input import Input
 
