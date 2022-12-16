8eee440b0a90648394524eb805d5e7e8b121a218
Jeff Holland <holland.musictech@gmail.com>
2022-12-16 13:34:52 -0500

    minor changes

--- src/info_bar.py
+++ src/info_bar.py
@@ -3,6 +3,11 @@ import tkinter as tk
 from colors import get_current_color_scheme
 from constants import *
 
+# Tool for debugging and general info viewing.
+# 
+# To turn it on: 
+# In main.py, set self.show_info_bar to True.
+
 class InfoBar(tk.Frame):
     def __init__(self, master):
 
